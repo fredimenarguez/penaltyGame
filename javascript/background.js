deb81eb9bf5467941163d78dc492e444d0cdfae6
Carlos <carlos_dic1985@hotmail.com>
2021-10-29 17:00:52 +0200

    backgroundBeta hecho

---
+++ javascript/background.js
@@ -0,0 +1,26 @@
+class Background {
+    constructor(ctx, posX, posY, width, height, imageName) {
+        this.ctx = ctx
+        this.posX = posX
+        this.posY = posY
+        this.width = width
+        this.height = height
+
+        this.imageInstance = undefined
+        this.imageName = imageName
+
+        this.init()
+
+    }
+
+    init() {
+        this.imageInstance = new Image()
+        this.imageInstance.src = `img/${this.imageName}`
+    }
+
+    draw() {
+        this.ctx.drawImage(this.imageInstance, this.posX, this.posY, this.width, this.height)
+    }
+
+
+}
--- javascript/game.js
+++ javascript/game.js
@@ -16,10 +16,75 @@ const game = {
     obstacles: [],
     keys: undefined,
 
+    init() {
+        this.setContext()
+        this.setDimensions()
+        this.setListeners()
+        this.createAll()
+        this.start()
+
+    },
+
+    setContext() {
+        this.canvasDOM = document.querySelector("#myCanvas")
+        this.ctx = this.canvasDOM.getContext("2d")
+    },
+
+    setDimensions() {
+        //TAMAÑO DONDE SE EJECUTARÁ EL JUEGO EN LA VENTANA. AHORA MISMO TODA LA VENTANA
+        this.canvasSize.width = window.innerWidth
+        this.canvasSize.height = window.innerHeight
+
+        //EL ATRIBUTO POR DEFECTO DE WIDTH Y HEIGHT
+        this.canvasDOM.setAttribute('width', this.canvasSize.width)
+        this.canvasDOM.setAttribute('height', this.canvasSize.height)
+
+    },
+    start() {
+        console.log('Start está funcionando')
+        setInterval(() => {
+            // this.clearScreen()
+            this.drawAll()
+            this.moveAll()
+            this.createAll()
+        }, 1000 / 1)
+    },
 
 
+    createAll() {
+        // this.createBall()
+        // this.createGKeaper()
+        this.createBackground()
+    },
 
+    createBackground() {
+        this.background = new Background(this.ctx, 0, 0, this.canvasSize.width, this.canvasSize.height, "background1.png")
+    },
 
+    drawAll() {
+        // this.drawBall()
+        // this.drawGKeaper()
+        this.drawBackground()
+    },
+    drawBackground() {
+        this.background.draw()
+    },
+
+    moveAll() {
+        // this.moveBall()
+        // this.moveGKeaper()
+
+    },
+
+    clearScreen() {
+        this.ctx.cleanRect(0, 0, this.canvasSize.width, this.canvasSize.height)
+    },
+
+    setListeners() {
+        document.onkeydown = (e) => {
+
+        }
+    },
 
 
 
