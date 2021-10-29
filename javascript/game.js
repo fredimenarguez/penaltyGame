2aba0659fe468649deac76076863bfc02828b3d3
Carlos <carlos_dic1985@hotmail.com>
2021-10-29 15:37:01 +0200

    estructura

---
+++ javascript/game.js
@@ -0,0 +1,32 @@
+const game = {
+    title: 'ChutaApuertA',
+    author: 'Alfredo & Carlos',
+    license: undefined,
+    version: '1.0.0',
+    description: 'Juego de penaltis',
+    canvasDOM: undefined,
+    ctx: undefined,
+    canvasSize: { width: undefined, height: undefined },
+    framesCounter: 0,
+    frames: 60,
+    intervalId: undefined,
+    background: undefined,
+    player: undefined,
+    scoreBoard: undefined,
+    obstacles: [],
+    keys: undefined,
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
